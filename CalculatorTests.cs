abb5d3e77c9ec8f51e059cd77ac986d388ac49da
Shaik Mahammad Gouse <59640213+shaik-byte@users.noreply.github.com>
2025-05-06 15:26:02 +0530

    Add files via upload

---
+++ CalculatorTests.cs
@@ -0,0 +1,18 @@
+using Xunit;
+
+public class CalculatorTests
+{
+    [Fact]
+    public void Add_ReturnsCorrectSum()
+    {
+        var calculator = new Calculator();
+        Assert.Equal(5, calculator.Add(2, 3));
+    }
+
+    [Fact]
+    public void Subtract_ReturnsCorrectDifference()
+    {
+        var calculator = new Calculator();
+        Assert.Equal(1, calculator.Subtract(3, 2));
+    }
+}
