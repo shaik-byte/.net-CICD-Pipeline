4a01ed78b9735bf02774a2d4610b92ad3fef9655
Shaik Mahammad Gouse <59640213+shaik-byte@users.noreply.github.com>
2025-05-06 15:01:29 +0530

    Add files via upload

---
+++ MyDotNetApp.cs
@@ -0,0 +1,5 @@
+namespace MyDotNetApp;
+public class Calculator
+{
+    public int Add(int a, int b) => a + b;
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,10 @@
+using Xunit;
+namespace MyDotNetApp.Tests;
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.Equal(4, 2 + 2);
+    }
+}
