f0ad7b5a25f680893e434642e9e7f73f8746dd01
Shaik Mahammad Gouse <59640213+shaik-byte@users.noreply.github.com>
2025-05-06 16:08:43 +0530

    Update Calculator.cs

--- lib/Calculator.cs
+++ lib/Calculator.cs
@@ -1,11 +1,10 @@
-<Project Sdk="Microsoft.NET.Sdk">
-
-  <PropertyGroup>
-    <TargetFramework>net7.0</TargetFramework>
-  </PropertyGroup>
-
-  <ItemGroup>
-    <Compile Include="lib/Calculator.cs" />
-  </ItemGroup>
-
-</Project>
+namespace CalculatorLib
+{
+    public class Calculator
+    {
+        public int Add(int a, int b) => a + b;
+        public int Subtract(int a, int b) => a - b;
+        public int Multiply(int a, int b) => a * b;
+        public int Divide(int a, int b) => a / b;
+    }
+}
